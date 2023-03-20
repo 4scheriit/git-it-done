2b7f69afe38df98ad995f7632aff3d572fbce39a
Maxwell Brown <mbrownet@gmail.com>
2023-03-20 19:35:41 -0400

    added function to fetch user data from github

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,15 @@
 "use strict";
 
-const getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+const getUserRepos = function (user) {
+  // format the github api url
+  let apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
 
-getUserRepos();
+getUserRepos("4scheriit");
