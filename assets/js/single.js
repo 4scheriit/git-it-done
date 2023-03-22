ac6cf46f085eb626789893fc10838e1858f07f22
Maxwell Brown <mbrownet@gmail.com>
2023-03-21 20:33:31 -0400

    made improvements incase more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let issueContainerEl = document.querySelector("#issues-container");
+let limitWarningEl = document.querySelector("#limit-warning");
 
 const getRepoIssues = function (repo) {
   let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,13 +10,29 @@ const getRepoIssues = function (repo) {
     // request was successful
     if (response.ok) {
       response.json().then(function (data) {
-        // pass response data to dom function
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
     }
   });
+};
+
+const displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  let linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
 };
 
 const displayIssues = function (issues) {
